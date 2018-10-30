3ff55b40f55190b1c6bc4393091510079472f96e
katejsenger <katejsenger@yahoo.com>
2018-10-30 16:54:03 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,17 +1,8 @@
 def my_each(array)
-end
-
-def hello_t(array)
-  if block_given?
-    i = 0
- 
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
- 
-    array
-  else
-    puts "Hey! No block was given!"
+  i = 0
+  while i < array.length
+# The "while" method will always return a value of => nil"
+    yield(array[i])
+    i = i + 1
   end
 end
